ec86bd2f2c8581ea56e5137b02b21eadbf0adf8f
Saurabh Zinjad <saurabh.zinjad@tigeranalytics.com>
2022-07-15 19:40:18 +0530

    ENH #7: Make project packageble and installable

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="mle_training_sz",
+    name="houseValuePredictionZ",
     version="0.0.1",
-    description="Basic ML training package",
-    author="saurabh-tiger",
+    description="House Value Prediction",
+    author="Saurabh Zinjad",
+    author_email="saurabh.zinjad@tigeranalytics.com",
+    license="LICENSE.txt",
     packages=find_packages(),
-    license="MIT",
+    long_description=open("README.md").read(),
 )
